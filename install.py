17773ec4f3965cb5a829fa51e93acf6b6beed84d
phamhungd <phamhung.d@gmail.com>
2023-07-12 03:21:52 +0700

    update

--- install.py
+++ install.py
@@ -8,13 +8,13 @@ req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requiremen
 
 import os
 
-models_dir = os.path.abspath('models/ghepmat2')
+models_dir = os.path.abspath('models/RoopSD')
 
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
-    print(f"ghepmat2 : You can put the model in {models_dir} directory")
+    print(f"RoopSD : You can put the model in {models_dir} directory")
 
-print("Check ghepmat2 requirements")
+print("Check RoopSD requirements")
 with open(req_file) as file:
     for package in file:
         try:
@@ -23,15 +23,15 @@ with open(req_file) as file:
 
             if not launch.is_installed(package):
                 print(f"Install {package}")
-                launch.run_pip(f'install {package}', f"ghepmat2 requirement: {package}")
+                launch.run_pip(f'install {package}', f"RoopSD requirement: {package}")
             elif '==' in package:
                 package_name, package_version = package.split('==')
                 installed_version = pkg_resources.get_distribution(package_name).version
                 if installed_version != package_version:
                     print(f"Install {package}")
-                    launch.run_pip(f'install {package}', f"ghepmat2 requirement: changing {package_name} version from {installed_version} to {package_version}")
+                    launch.run_pip(f'install {package}', f"RoopSD requirement: changing {package_name} version from {installed_version} to {package_version}")
                 
         except Exception as e:
             print(e)
-            print(f'Warning: Failed to install {package}, ghepmat2 will not work.')            
+            print(f'Warning: Failed to install {package}, RoopSD will not work.')            
             raise e
